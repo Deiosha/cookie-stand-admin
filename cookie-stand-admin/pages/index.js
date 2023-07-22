a4cbae9f59831c2013fc4274ef297c1f420941b6
DEIOSHA SPARKS <DEIOSHASPARKS13@GMAIL.COM>
2023-07-22 12:34:16 -0700

    better placement

--- cookie-stand-admin/pages/index.js
+++ cookie-stand-admin/pages/index.js
@@ -6,49 +6,38 @@ export default function Home() {
       <Head>
         <title>Cookie Stand Admin</title>
       </Head>
-      <header className="flex item-center justify-between bg-green-600 text-black-80">
+      <header className=" bg-green-300 text-black-80 justify-between">
         <h1>Cookie Stand Admin</h1>
       </header>
-      <main className="flex flex-col items-center p-4">
-        <div className="w-full max-w-xl border border-green-500 bg-green-400 flex flex-col items-center">
-          <h2 className="font-bold mb-8">Create Cookie Stand</h2>
-          <div className="w-full flex mb-4">
-            <label className="text-black w-2/4">Location:</label>
-            <input
-              className=" bg-white w-4/4 text-black-700 py-2 px-2"
-              type="text"
-            />
-          </div>
-          <div className="w-full flex mb-2 justify-between">
-            <div className="w-1/3">
-              <label className="text-black">Minimum Cookies per Hour:</label>
-              <input
-                className=" bg-white w-full text-gray-700 py-2 px-2"
-                id="minCookies"
-              />
+
+      <main className="m-4">
+          <form className="bg-green-200 flex flex-col justify-center border-2 border-green-950 w-7/12 m-auto rounded-lg">
+            <h2 className='text-5xl text-center'>Create Cookie Stand</h2>
+            <div className="flex justify-around p-3 my-4">
+              <label>Location</label>
+              <input className="flex-auto pl-1"/>
             </div>
-            <div className="w-1/3">
-              <label className="text-black"> Maximum Cookies per Hour:</label>
-              <input
-                className=" bg-white w-full text-gray-700 py-2 px-2"
-              />
+            <div className="flex justify-between">
+              <div>
+                <label className="display: inline-block;padding: 0.25rem; ">Min Customers</label>
+               
+                <input className="display: inline-block;padding: 0.25rem;"/>
+              </div>
+              <div>
+                <label className="display: inline-block;padding: 0.25rem;">Max Customers</label>
+                
+                <input className="display: inline-block;padding: 0.25rem;"/>
+              </div>
+              <div>
+                <label className="display: inline-block;padding: 0.25rem;">Average Cookies</label>
+                
+                <input className="display: inline-block;padding: 0.25rem;"/>
+              </div>
+                <button className="display: inline-block;padding: 0.25rem; border-green-950 rounded bg-green-500 w-28">Create</button>
             </div>
-            <div className="w-1/3">
-              <label className="text-black ">Average Cookies per Hour:</label>
-              <input
-                className=" bg-white w-full text-black-700 py-2 px-2"
-              />
-            </div>
-          </div>
-          <button
-            className=" text-black border-2 border-green-950 rounded bg-green-500 w-28"
-            type="button" >
-            Create
-          </button>
-        </div>
-        <div className="mt-4">
-          <p className="text-black">Report Table Coming Soon...</p>
-        </div>
+          </form>
+
+
       </main>
       <footer className=" bg-green-500 p-4 mt-4">
         <p>&copy;2023</p>
